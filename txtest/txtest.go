a875da3638a2e4257deb49d598dbd5e400139c72
Myron Rotter <myronrotter01@t-online.de>
2025-02-11 08:58:28 +0100

    feat: add tx manager for testing

---
+++ txtest/txtest.go
@@ -0,0 +1,18 @@
+package txtest
+
+import (
+	"context"
+	"tx"
+)
+
+type Manager struct{}
+
+var _ tx.Manager = &Manager{}
+
+func New() *Manager {
+	return &Manager{}
+}
+
+func (m *Manager) DoInTransaction(ctx context.Context, uow func(ctx context.Context) error) error {
+	return uow(ctx)
+}
